5957339dd2633c5ce0210366ee754e0f9773bef0
Hubert Lefevre <hlefevre@netatmo.com>
2016-09-09 10:30:11 +0200

    script: Add lots of script

---
+++ scripts/cortex_log.py
@@ -0,0 +1,245 @@
+# !/usr/bin/python
+
+# Inspired from the script to highlight adb logcat output for console
+# written by Jeff Sharkey (http://jsharkey.org/)
+#
+# This scripts get what on a specified tty serial port parse it and color it.
+# The input format will be in the format "[TAG] MESSAGE" by line.
+# The output will add the time of the reception in front of the line and TAG
+# will be colored with a color that won't change for a given 'TAG'.
+#
+# Written by Hubert Lefevre.
+
+import os, sys, re, StringIO
+import fcntl, termios, struct
+import datetime
+import getopt
+import serial
+
+def format(fg=None, bg=None, bright=False, bold=False, dim=False, reset=False):
+    # manually derived from http://en.wikipedia.org/wiki/ANSI_escape_code#Codes
+    codes = []
+    if reset:
+        codes.append("0")
+    else:
+        if not fg is None:
+            codes.append("3%d" % (fg))
+        if not bg is None:
+            if not bright:
+                codes.append("4%d" % (bg))
+            else:
+                codes.append("10%d" % (bg))
+        if bold:
+            codes.append("1")
+        elif dim:
+            codes.append("2")
+        else:
+            codes.append("22")
+    return "\033[%sm" % (";".join(codes))
+
+def usage():
+    print "cortex_log.py <tty> [-w <nocolor>]"
+    print " or"
+    print "<source> | cortex_log.py [-w <nocolor>]"
+    print ""
+    print "   <tty> Specify which tty to use, the script will use "\
+          "\'/dev/tty<tty>\'"
+    print "    -w <path>       Write output in a file (nocolor)"
+    sys.exit(2)
+
+
+#############
+# Constants #
+#############
+# Unpack the current terminal width/height
+data = fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ, '1234')
+HEIGHT, WIDTH = struct.unpack('hh',data)
+
+# Colors
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# Tag color allocation context
+LAST_USED = [RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE]
+KNOWN_TAGS = {}
+
+# Width setting
+TIME_WIDTH  = 19 # Hard coded size of the time format e.g. '08-31 10:55:02.357'
+TAG_WIDTH   = 10
+HEADER_SIZE = TAG_WIDTH + TIME_WIDTH
+
+# Regular expression to match
+rebootline  = re.compile("^\(@\)(.*) (.*)$")
+rerbootline = re.compile("^\r\(@\)(.*) (.*)$")
+rematch     = re.compile("^\[([^\(]+?)\] (.*)$")
+rermatch    = re.compile("^\r\[([^\(]+?)\] (.*)$")
+retagonly   = re.compile("^\[([^\(]+?)\](.*)$")
+rertagonly  = re.compile("^\r\[([^\(]+?)\](.*)$")
+resimple    = re.compile("^([^\(]+)([^\(]+)$")
+rersimple   = re.compile("^\r([^\(]+)([^\(]+)$")
+
+# Format function
+def print_time(linebuf, nocolor):
+    linebuf.write("%s%s %s" % (format(fg=GREEN, bg=BLACK, dim=False),
+                               datetime.datetime.now().strftime(
+                                   "%m-%d %H:%M:%S.%f")[:-3],
+                               format(reset=True)))
+    nocolor.write("%s " % (datetime.datetime.now().strftime(
+                               "%m-%d %H:%M:%S.%f")[:-3]))
+
+def allocate_color(tag):
+    # this will allocate a unique format for the given tag
+    # since we dont have very many colors, we always keep track of the LRU
+    if not tag in KNOWN_TAGS:
+        KNOWN_TAGS[tag] = LAST_USED[0]
+    color = KNOWN_TAGS[tag]
+    LAST_USED.remove(color)
+    LAST_USED.append(color)
+    return color
+
+def print_tag(linebuf, nocolor, tag, bootline=False):
+    # right-align tag title and allocate color if needed
+    tag   = tag.strip()
+    if bootline:
+        color = RED
+    else:
+        color = allocate_color(tag)
+    tag = tag[-TAG_WIDTH:].rjust(TAG_WIDTH)
+    linebuf.write("%s%s %s" % (format(fg=color, dim=False), tag,
+                               format(reset=True)))
+    nocolor.write("%s " % (tag))
+
+def print_message(linebuf, nocolor, headersize, message, bootline=False):
+    # insert line wrapping as needed
+    wrap_area = WIDTH - headersize - 3
+    current = 0
+    while current < len(message):
+        next = min(current + wrap_area, len(message))
+        linebuf.write("%s %s %s" % (format(bg=BLACK, dim=False),
+                                    format(reset=True), message[current:next]))
+        nocolor.write("  %s" % (message[current:next]))
+
+        if bootline:
+            linebuf.write("%s " % (format(reset=True)))
+            nocolor.write(" ")
+
+        if next < len(message):
+            linebuf.write("\n%s " % (" " * headersize))
+            nocolor.write("\n%s " % (" " * headersize))
+        current = next
+
+########
+# Main #
+########
+
+# Context
+writefile = None;
+
+# If someone is piping in to us, use stdin as input, otherwise use first
+# argument to know which tty you should open
+if os.isatty(sys.stdin.fileno()):
+    if len(sys.argv) < 2:
+        usage()
+    source              = serial.Serial()
+    source.port         = "/dev/tty%s" % sys.argv[1]
+    ########################
+    # Serial configuration #
+    ########################
+    source.baudrate     = 115200
+    source.bytesize     = serial.EIGHTBITS
+    source.parity       = serial.PARITY_NONE
+    source.stopbits     = serial.STOPBITS_ONE
+    source.timeout      = 0     # Non-Block reading
+    source.xonxoff      = False # Disable Software Flow Control
+    source.rtscts       = False # Disable (RTS/CTS) flow Control
+    source.dsrdtr       = False # Disable (DSR/DTR) flow Control
+    source.timeout      = None  # Wait forever
+    source.writeTimeout = 2
+    try:
+        source.open()
+    except serial.SerialException as err:
+        print str(err)
+        print ""
+        usage()
+    source.flushInput()
+    source.flushOutput()
+else:
+    source = sys.stdin
+
+# Handle options
+try:
+    opts, arg = getopt.getopt(sys.argv[2:], "hw:", ["help"])
+except getopt.GetoptError as err:
+    print str(err)
+    print ""
+    usage()
+
+
+for o, a in opts:
+    if o in ("-h", "--help"):
+        usage()
+    elif o == "-w":
+        writefile = open(a, 'a')
+        if not writefile is None:
+            print "File opened"
+        else:
+            print "Error opening file"
+            print ""
+            usage()
+
+# Main loop
+while True:
+    try:
+        line = source.readline()
+    except KeyboardInterrupt:
+        break
+
+    isbootline = False
+    bootline = rebootline.match(line)
+    if bootline is None:
+        bootline = rerbootline.match(line)
+
+    match = rermatch.match(line)
+    if match is None:
+        match = rematch.match(line)
+
+    tagonly = rertagonly.match(line)
+    if tagonly is None:
+        tagonly = retagonly.match(line)
+
+    simple = rersimple.match(line)
+    if simple is None:
+        simple = resimple.match(line)
+
+    if not bootline is None:
+        tag, message = bootline.groups()
+        isbootline   = True
+
+    elif not match is None:
+        tag, message = match.groups()
+
+    elif not tagonly is None:
+        tag, uu = tagonly.groups()
+        message = " "
+
+    elif not simple is None:
+        message, uu = simple.groups()
+        tag         = " "
+
+    elif len(line) == 0:
+        break;
+
+    else:
+        print(line)
+        if not writefile is None:
+            writefile.write(line)
+        continue
+
+    linebuf = StringIO.StringIO()
+    nocolor = StringIO.StringIO()
+
+    print_time(linebuf, nocolor)
+    print_tag(linebuf, nocolor, tag, isbootline)
+    print_message(linebuf, nocolor, HEADER_SIZE, message, isbootline)
+    print linebuf.getvalue()
+    if not writefile is None:
+        writefile.write(nocolor.getvalue() + '\n');
---
+++ scripts/split_git.sh
@@ -0,0 +1,145 @@
+#!/bin/sh
+
+display_usage() { 
+    echo "\nUsage:\n$0 [-s|--source=REPO_SOURCE] [-d|--destination=REPO_DESTINATION] [--branch-regex=BRANCH_REGEX] [--tag-regex=TAG_REGEX] [-f|--filter=FILTER_FOLDER] [-r|--rm-folders=RM_FOLDERS] \n"
+    exit 0
+}
+
+run=
+
+current_dir=`pwd`
+
+for i in "$@"
+do
+case $i in
+    -s=*|--source=*)
+    REPO_SOURCE="${i#*=}"
+    shift # past argument=value
+    ;;
+    --branch-regex=*)
+    BRANCH_REGEX="${i#*=}"
+    shift # past argument=value
+    ;;
+    --tag-regex=*)
+    TAG_REGEX="${i#*=}"
+    shift # past argument=value
+    ;;
+    -f=*|--filter=*)
+    FILTER_FOLDER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -d=*|--destination=*)
+    REPO_DESTINATION="${i#*=}"
+    shift # past argument=value
+    ;;
+    -r=*|--rm-folders=*)
+    RM_FOLDERS="${i#*=}"
+    shift # past argument=value
+    ;;
+    -n|--dry-run)
+    run=echo
+    ;;
+    -h=*|--help=*)
+    display_usage
+    ;;
+    *)
+    echo "Unknown argument $i"
+    ;;
+esac
+done
+
+if [ -z "$REPO_SOURCE" ]; then echo "Missing REPO_SOURCE argument."; display_usage; else echo "REPO_SOURCE          = ${REPO_SOURCE}"; fi
+if [ -z "$REPO_DESTINATION" ]; then echo "Missing REPO_DESTINATION argument."; display_usage; else echo "REPO_DESTINATION   = ${REPO_DESTINATION}"; fi
+if [ -z "$FILTER_FOLDER" ]; then echo "Missing FILTER_FOLDER argument."; display_usage; else echo "FILTER_FOLDER        = ${FILTER_FOLDER}"; fi
+
+# Create temp folder
+tmp_dir=`mktemp -d`
+
+echo "###### Cloning in : ${tmp_dir}"
+cd $tmp_dir
+
+# Clone source repo
+git clone $REPO_SOURCE netatmo
+
+cd ./netatmo
+
+# List all remote branches and filter using regex
+if [ -z "$BRANCH_REGEX" ]; then
+    echo "Keeping all branches."; 
+    BRANCHES=`git branch --list --remote | tr -d '[[ ]]' | sed "s/^origin\///"`
+else 
+    echo "Filter branches with regex ${BRANCH_REGEX}"
+    BRANCHES=`git branch --list --remote | egrep $BRANCH_REGEX | tr -d '[[ ]]' | sed "s/^origin\///"`
+fi
+
+# Track all branches locally
+for branch in $BRANCHES; do
+    $run git branch -t $branch origin/$branch
+done
+
+# Remove old origin
+git remote rm origin
+
+if [ -z "$TAG_REGEX" ]; then
+    echo "Keeping all tags."; 
+else 
+    # Filter tags using inverted regex
+    echo "Filter tags with regex ${TAG_REGEX}"
+    TAGS=`git tag | egrep -v $TAG_REGEX`
+
+    # For each rejected tags, delete it
+    for tag in $TAGS; do
+        $run git tag -d $tag
+    done
+fi
+
+# Actually filter the repo
+$run git filter-branch --tag-name-filter cat --prune-empty --subdirectory-filter $FILTER_FOLDER -- --all
+
+$run git reset --hard
+
+# Remove backup
+ORIGINALS=`git for-each-ref --format="%(refname)" refs/original/`
+for original in $ORIGINALS; do
+    $run git update-ref -d $original
+done
+
+if [ -z "$RM_FOLDERS" ]; then
+    echo "Keeping all folders."; 
+else 
+    # Filter tags using inverted regex
+    echo "Filter folders : ${RM_FOLDERS}"
+    $run git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch ${RM_FOLDERS}" --prune-empty --tag-name-filter cat -- --all
+fi
+
+$run git reset --hard
+
+# Remove backup
+ORIGINALS=`git for-each-ref --format="%(refname)" refs/original/`
+for original in $ORIGINALS; do
+    $run git update-ref -d $original
+done
+
+# Clean up the mess
+$run git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
+$run git reflog expire --expire=now --all
+$run git gc --aggressive --prune=now
+
+# Add new origin
+$run git remote add origin $REPO_DESTINATION
+
+# push master branch
+$run git push -u origin master
+
+# push all local branches
+for branch in $BRANCHES; do
+    $run git push -u origin $branch
+done
+
+# Push tags
+$run git push --tags
+
+cd $current_dir
+
+# remove temp repository
+# rm -rf $tmp_dir
