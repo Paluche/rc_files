740b458c0acadde1d69e64d16e0335089324f096
Hubert Lefevre <hlefevre@netatmo.com>
2019-08-12 16:08:59 +0200

    vim: Update Java skeleton

--- vim_rc/vim/skeletons/template.java
+++ vim_rc/vim/skeletons/template.java
@@ -1,15 +1,19 @@
 /*
  * (@) <+COMPANY+> <+DATE+>
- * <+AUTHOR+>
  *
  * <+CURSOR+>
  */
 package <+JAVA_PACKAGE+>;
 
+
 public class <+FILE_NAME_ROOT+>
 {
+    // Constants
     private static final String LOG_TAG = "<+FILE_NAME_ROOT+>";
 
+    /**
+     * @brief Class constructor
+     */
     public <+FILE_NAME_ROOT+>()
     {
     }
